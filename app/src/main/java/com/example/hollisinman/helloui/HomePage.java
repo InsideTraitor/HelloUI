ef8fe924ee262b385447ad00cf7ae9b7e5532973
InsideTraitor <hollis@droid.rocks>
2017-12-17 15:00:19 -0500

    Added bunch of resources to VCS

---
+++ app/src/main/java/com/example/hollisinman/helloui/HomePage.java
@@ -0,0 +1,35 @@
+package com.example.hollisinman.helloui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity implements View.OnClickListener{
+
+    TextView welcome;
+    Button goBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        welcome = findViewById(R.id.txt_welcome);
+        goBack = findViewById(R.id.btn_go_back);
+
+        goBack.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_go_back:
+                Intent loginPage = new Intent(this, Login.class);
+                startActivity(loginPage);
+        }
+    }
+}
